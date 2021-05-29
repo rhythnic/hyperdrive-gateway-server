6b5d942b983a6f1d9094e037c2ea52a6a5848ae8
Nicholas Baroni <nbaroni@tuta.io>
2021-05-30 00:20:54 +0900

    Cleanup HyperdriveController

--- controllers/hyperdrive.js
+++ controllers/hyperdrive.js
@@ -4,6 +4,8 @@ import { extname } from 'path'
 
 export class HyperdriveController {
   static PAGE_DEPENDENCY_REGEX = /(src|href)=["']hyper:\/\/(.+)["']/g
+
+  static GATEWAY_ROUTE_REGEX = /^(\/hyper)\/([0-9a-fA-F]{64})(\/?.*)$/
   
   static NETWORK_CONFIG = {
     announce: false,
@@ -16,7 +18,7 @@ export class HyperdriveController {
 
   async handleRequest(req, res, url) {
     if (req.method === 'GET') {
-      let match = /^(\/hyper)\/([0-9a-fA-F]{64})(\/?.*)$/.exec(url.pathname)
+      let match = new RegExp(this.constructor.GATEWAY_ROUTE_REGEX).exec(url.pathname)
       if (match) {
         await this.serveHyperdriveFile(req, res, match, url)
         return true
@@ -27,13 +29,14 @@ export class HyperdriveController {
 
   async serveHyperdriveFile(req, res, params) {
     let [_, basePath, publicKey, filePath] = params
-    filePath = !filePath || filePath === '/' ? '/index.html' :filePath
+    filePath = !filePath || filePath === '/' ? '/index.html' : filePath
     try {
       const drive = new Hyperdrive(this.client.corestore(), Buffer.from(publicKey, 'hex'))
       await drive.promises.ready()
-      await this.client.network.configure(drive.discoveryKey, this.constructor.DRIVE_NETWORK_CONFIG)
+      await this.client.network.configure(drive.discoveryKey, this.constructor.NETWORK_CONFIG)
       const body = await drive.promises.readFile(filePath, 'utf8')
-      res.setHeader('Content-Type', mime.contentType(extname(filePath)))
+      const contentType = mime.contentType(extname(filePath))
+      if (contentType) res.setHeader('Content-Type', contentType)
       res.statusCode = 200
       res.end(body.replace(
         this.constructor.PAGE_DEPENDENCY_REGEX,
